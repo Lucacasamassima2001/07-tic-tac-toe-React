566b7c83c7dcdc8853ba5a3183bee17a06bb9c41
Luca Casamassima <casamassimaluca723@gmail.com>
2024-01-09 15:37:55 +0100

    Player component & bidimensional array fro the grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import GameBoard from "./Components/Gameboard";
+import Player from "./Components/Player";
+
 function App() {
-  return <h1>React Tic-Tac-Toe</h1>;
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player initialName={"Player 1"} symbol={"X"}></Player>
+          <Player initialName={"Player 2"} symbol={"O"}></Player>
+        </ol>
+        <GameBoard></GameBoard>
+      </div>
+    </main>
+  );
 }
 
 export default App;
---
+++ src/Components/Player.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  function handleEditClick() {
+    // prev (prendere lo stato precedente per star sicuri che sia quello corretto)
+    setIsEditing((editing) => !editing);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" onChange={handleChange} required value={playerName} />
+    );
+  }
+
+  return (
+    <>
+      <li>
+        <span className="player">
+          {editablePlayerName}
+          <span className="player-symbol">{symbol}</span>
+        </span>
+        <button className="button" onClick={handleEditClick}>
+          {isEditing ? "Save" : "Edit"}
+        </button>
+      </li>
+    </>
+  );
+}
